a41e7eb1cc958ecef44a6eb3ec52b1e412573222
Jenny Khranovska <jenny.khranovska.syne25lin@edu.tucsweden.se>
2025-09-18 12:46:04 +0200

    Add project files.

---
+++ Car2/Car2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car2
+{
+    internal class Car2
+    {
+        public string Brand;
+        public string Color;
+        public int MaxSpeed;
+        public int HorsePower;
+        public CarType CarType;
+
+
+
+
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Carinfo: {Brand}, {Color}, {MaxSpeed}");
+        }
+        public void Drive()
+        {
+            Console.WriteLine($"The {Brand} is driving at MaxSpeed {MaxSpeed}");
+      
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine("Honk Honk");
+                
+        }
+        public void Fuel()
+
+        {
+            Console.WriteLine($"Car is of type: {CarType}");
+            switch (CarType)
+            {
+                case CarType.Electric:
+                    Console.WriteLine("electric fuel");
+                    break;
+
+                case CarType.Gas:
+                    Console.WriteLine("gas fuel");
+                    break;
+
+                case CarType.Disel:
+                    Console.WriteLine("disel fuel");
+                    break;
+
+                case CarType.Hybrid:
+                    Console.WriteLine("hybrid fuel");
+                    break;
+
+            }
+
+        }
+                 
+    }
+
+}
---
+++ Car2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Car2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car2 ferrari = new Car2();
+            ferrari.Color = "Red";
+            ferrari.Brand = "Ferrari";
+            ferrari.MaxSpeed = 380;
+            ferrari.HorsePower = 500;
+            ferrari.CarType = CarType.Gas;
+
+            Car2 volvo = new Car2();
+            volvo.Color = "Green";
+            volvo.Brand = " Volvo";
+            volvo.MaxSpeed = 350;
+            volvo.HorsePower = 450;
+            volvo.CarType = CarType.Disel;
+
+
+            ferrari.ShowInfo();
+            ferrari.Drive();
+            ferrari.Honk();
+            ferrari.Fuel();
+
+
+            volvo.ShowInfo();
+            volvo.Fuel();
+            volvo.Honk();
+            volvo.Drive();
+        }
+
+
+    }
+}
